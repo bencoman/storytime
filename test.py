f6280ec7f29a353f7a662f7ff334c6c95f20233b
Ben Coman <benjamin.t.coman@gmail.com>
2025-05-11 21:19:17 +0800

    deleted legacy test.py file

--- test.py
+++
@@ -1,4 +0,0 @@
-# This file has been deprecated. Tests have been migrated to test_client.py for pytest compatibility.
-# Logs are now stored in the 'log' directory.
-# Please use pytest to run the tests.
-
